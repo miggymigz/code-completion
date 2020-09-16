928b52b0a397c5cb91c056a34543d3157a0bccd0
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-09-16 11:42:04 +0800

    Refactored download repos code

--- ccompletion/download_utils.py
+++ ccompletion/download_utils.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from tqdm import tqdm
+from typing import Optional
 
 import codecs
 import csv
@@ -13,7 +14,6 @@ import tensorflow as tf
 API_BASE_URL = 'https://api.github.com'
 REPO_INFO_API = '/repos/{}/{}'
 REPO_ZIP_URL = 'https://github.com/{}/{}/archive/{}.zip'
-EXCLUDED_PATTERN = re.compile(r'(?:__init__\.py)|(?:test.+\.py)')
 
 
 def get_latest_release_url(user, name, access_token=None):
@@ -57,11 +57,7 @@ def get_header_for_auth(access_token=None):
     return {'Authorization': auth_value}
 
 
-def get_repo_list(name='repositories.csv', star_count_threshold=1000):
-    # ensure csv file exists
-    if not os.path.isfile(name):
-        raise FileNotFoundError(f'"{name}" does not exist.')
-
+def get_repo_list(name: Path, star_count_threshold: int):
     # the resulting repository list container
     repositories = []
 
@@ -85,86 +81,70 @@ def get_repo_list(name='repositories.csv', star_count_threshold=1000):
     return repositories
 
 
-def collate_python_files(user, name, access_token=None):
-    # ensure "repositories" directory exists
-    if not os.path.isdir('repositories'):
-        os.mkdir('repositories')
+def collate_python_files(reponame: str, output_path: Path, access_token: Optional[str]):
+    # split repo name into its username and repository name
+    # e.g., donnemartin/system-design-primer
+    user, name = reponame.split('/', 1)
+
+    # ensure output directory exists
+    output_path.mkdir(parents=False, exist_ok=True)
 
     # ensure parent directory for the user exists
-    user_dir = os.path.join('repositories', user)
-    if not os.path.isdir(user_dir):
-        os.mkdir(user_dir)
+    user_path = output_path / user
+    user_path.mkdir(parents=False, exist_ok=True)
 
     # skip if repository is already downloaded and extracted
     # empty directories are okay because there are repositories
     # that does not contain any python script (or were filtered as unwanted)
-    container_dir = os.path.join('repositories', user, name)
-    if os.path.isdir(container_dir):
+    container_path = user_path / name
+    if container_path.exists():
         return
 
     # download repo's default branch and preserve python files
     url, _ = get_latest_release_url(user, name, access_token=access_token)
     filename = f'{user}_{name}.zip'
-    path = tf.keras.utils.get_file(
+    path = Path(tf.keras.utils.get_file(
         filename,
         origin=url,
         extract=True,
         cache_dir='.keras',
         cache_subdir='code-completion-repos',
-    )
+    ))
 
     # determine extracted directory name
-    cache_dir_path = os.path.dirname(path)
     _dirs = [
         f
-        for f in os.listdir(cache_dir_path)
-        if os.path.isdir(os.path.join(cache_dir_path, f))
+        for f in path.parent.glob('*')
+        if f.is_dir()
     ]
     assert len(_dirs) == 1, "More directories were found in extracted dir"
-    extracted_dir_path = os.path.join(
-        cache_dir_path,
-        _dirs[0],
-    )
+    extracted_dir_path = path.parent / _dirs[0]
 
     # filter out python source files and move it to 'repositories' directory
     extract_python_src_files(
         user,
         name,
         extracted_dir_path,
-        exclude_tests=False,
+        output_path=output_path,
     )
 
     # remove downloaded zip
-    os.remove(path)
-    print()
+    path.unlink()
 
 
-def extract_python_src_files(user, name, path, exclude_tests=True):
+def extract_python_src_files(user: str, name: str, path: Path, output_path: Path):
     # ensure extracted zip directory exists
-    assert os.path.isdir(path), f"{path} directory does not exist!"
+    assert path.exists(), f"{path} directory does not exist!"
 
     # ensure target source files container directory exists
-    container_directory = os.path.join('repositories', user, name)
-    if not os.path.isdir(container_directory):
-        os.mkdir(container_directory)
+    container_path = output_path / user / name
+    container_path.mkdir(parents=False, exist_ok=False)
 
     # move all candidate python source files to the target container directory
-    for root, dirs, files in os.walk(path):
-        # remove all test directories
-        if exclude_tests:
-            dirs[:] = [d for d in dirs if not d.lower().startswith('test')]
-
-        for f in files:
-            # filter out non-python source files
-            if not f.endswith('.py'):
-                continue
-
-            # include all source files if exclude_tests is False
-            # filter out __init__.py or test*.py if exclude_tests is True
-            if not exclude_tests or (exclude_tests and not re.match(EXCLUDED_PATTERN, f)):
-                file_src_path = os.path.join(root, f)
-                file_dst_path = os.path.join(container_directory, f)
-                shutil.move(file_src_path, file_dst_path)
+    for f in path.glob('**/*'):
+        # only retain Python source files
+        if str(f).endswith('.py'):
+            shutil.move(f, container_path / f.name)
 
     # delete original project directory
     shutil.rmtree(path)
--- download-repositories.py
+++ download-repositories.py
@@ -1,4 +1,6 @@
 from ccompletion.download_utils import get_repo_list, collate_python_files
+from pathlib import Path
+from tqdm import tqdm
 from typing import Optional
 
 import fire
@@ -7,19 +9,26 @@ import sys
 
 def download(
     repositories: str = 'repositories.csv',
+    output_dir: str = 'repositories',
     access_token: Optional[str] = None,
-    threshold: int = 1000
+    threshold: int = 500,
 ):
+    repos_csv_path = Path(repositories)
+    repos_output_path = Path(output_dir)
+
+    # get the list of repositories to be downloaded
     repo_list = get_repo_list(
-        name=repositories,
+        name=repos_csv_path,
         star_count_threshold=threshold,
     )
 
-    for reponame in repo_list:
-        user, name = reponame.split('/', 1)
-
+    for reponame in tqdm(repo_list):
         try:
-            collate_python_files(user, name, access_token=access_token)
+            collate_python_files(
+                reponame,
+                output_path=repos_output_path,
+                access_token=access_token,
+            )
         except (OSError, AssertionError) as e:
             print(f'WARN - Could not unpack {reponame}')
             print(e)
