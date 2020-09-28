8b3dd5512f3f4141eb1e3b0a5e87212ce1c2eaa0
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-09-28 20:39:14 +0800

    Completed finetuning script

--- dataset.py
+++ dataset.py
@@ -1,21 +1,65 @@
+from collections import defaultdict
 from pathlib import Path
 from torch.utils.data import IterableDataset
+from typing import Callable
 
 import math
-import torch
 
 
 class PythonReposDataset(IterableDataset):
-    def __init__(self, dataset_dir: str = 'repositories'):
-        # initialize dataset directory path
+    def __init__(
+        self,
+        dataset_dir: str = 'repositories',
+        count_fn: Callable[[str], int] = None,
+        batch_size: int = 8,
+        max_position: int = 1024,
+    ):
+        self.count_fn = count_fn
+        assert count_fn is not None
+
         self.dataset_path = Path(dataset_dir)
         assert self.dataset_path.is_dir()
 
-    def __iter__(self):
-        for f in self.dataset_path.rglob('*.py'):
-            with f.open('r', encoding='utf-8') as fd:
-                code = fd.read().strip()
-                yield code
+        self.batch_size = batch_size
+        self.max_position = max_position
+        self.source_files = list(self.dataset_path.rglob('*.py'))
 
-    def __len__(self):
-        return len(list(self.dataset_path.rglob('*.py')))
+        # cache for storing filenames and their corresponding token counts
+        self.buckets = defaultdict(list)
+
+    def __iter__(self) -> str:
+        # send batches of width <= `max_position`
+        batch = []
+        for i in range(len(self.source_files)):
+            # count tokens of the current source file
+            src = self.read_contents(i)
+            count = self.count_fn(src)
+
+            # append source file contents to the current batch
+            # if its token length is less than `max_position`
+            if count < self.max_position:
+                batch.append(src)
+            # store count of current source file in a bucket
+            # in preparation for later dispatching
+            else:
+                bucket_id = math.ceil(count / self.max_position) - 1
+                self.buckets[bucket_id].append(i)
+
+            # return batch if it is already complete
+            # also, empty list in preparation for the next batch
+            if len(batch) == self.batch_size:
+                yield batch
+                batch = []
+
+        # send remaining batches (inside buckets)
+        for _, indices in self.buckets.items():
+            n_batches = math.ceil(len(indices) / self.batch_size)
+            for i in range(n_batches):
+                batch = indices[i * self.batch_size: i *
+                                self.batch_size + self.batch_size]
+                yield [self.read_contents(j) for j in batch]
+
+    def read_contents(self, index) -> str:
+        f = self.source_files[index]
+        with f.open('r', encoding='utf-8') as fd:
+            return fd.read().strip()
--- finetune.py
+++ finetune.py
@@ -1,17 +1,22 @@
-from torch.utils.data.dataloader import DataLoader
-from transformers import GPT2LMHeadModel, GPT2TokenizerFast, AdamW
-from tqdm import tqdm
-
+import torch
+from transformers import data
 from dataset import PythonReposDataset
+from pathlib import Path
+from transformers import GPT2LMHeadModel, GPT2TokenizerFast, AdamW
 
 import fire
 import math
 
 
-def finetune(variant: str = 'gpt2', dataset_dir: str = 'repositories', batch_size: int = 8):
+def finetune(
+    gpt2_variant: str = 'gpt2',
+    checkpoint_dir: str = 'checkpoints',
+    dataset_dir: str = 'repositories',
+    batch_size: int = 8
+):
     # instantiate pretrained tokenizer and model
-    model = GPT2LMHeadModel.from_pretrained(variant)
-    tokenizer = GPT2TokenizerFast.from_pretrained(variant)
+    model = GPT2LMHeadModel.from_pretrained(gpt2_variant)
+    tokenizer = GPT2TokenizerFast.from_pretrained(gpt2_variant)
 
     # set model to training mode
     model.train()
@@ -22,31 +27,55 @@ def finetune(variant: str = 'gpt2', dataset_dir: str = 'repositories', batch_siz
     tokenizer.pad_token = tokenizer.eos_token
 
     # retrieve python repositories dataset
-    dataset = PythonReposDataset(dataset_dir=dataset_dir)
-    dataloader = DataLoader(dataset, batch_size=batch_size)
+    n_positions = model.config.n_positions
+    dataset = PythonReposDataset(
+        dataset_dir=dataset_dir,
+        batch_size=batch_size,
+        count_fn=lambda x: len(tokenizer(x)['input_ids']),
+        max_position=n_positions,
+    )
+
+    # initialize model's optimizer
+    optimizer = AdamW(model.parameters(), lr=1e-5)
 
     # The goal of this finetuning is to let the model see each of the python source
     # files exactly once (and not by epochs)
-    n_total_steps = math.ceil(len(dataset) / batch_size)
-    pbar = tqdm(enumerate(dataloader), total=n_total_steps)
-    for i, batch in pbar:
+    for i, batch in enumerate(dataset):
         # encode batch into their token IDS
         encoding = tokenizer(
             batch, return_tensors='pt',
-            padding=True, truncation=True,
+            padding=True
         )
         input_ids = encoding['input_ids']
         attn_mask = encoding['attention_mask']
 
-        # compute loss
-        optimizer = AdamW(model.parameters(), lr=1e-5)
-        loss = model(input_ids, attention_mask=attn_mask, labels=input_ids)[0]
-        pbar.write(f'Batch {i:3d}: Loss={loss}')
+        # split tensors since the model has a max length limit
+        input_ids = torch.split(input_ids.transpose(0, 1), n_positions)
+        attn_mask = torch.split(attn_mask.transpose(0, 1), n_positions)
+        past = None
 
-        # update parameters
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        for j in range(len(input_ids)):
+            _input_ids = input_ids[j].transpose(0, 1)
+            _attn_mask = attn_mask[j].transpose(0, 1)
+
+            # compute loss
+            loss, _, past = model(
+                _input_ids,
+                attention_mask=_attn_mask,
+                past_key_values=past,
+                labels=_input_ids,
+            )
+            print(f'Batch {i+1:3d}: Loss={loss}')
+
+            # update parameters
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    # save finetuned weights
+    ckpt_path = Path(checkpoint_dir) / 'gpt2-finetuned'
+    model.save_pretrained(ckpt_path)
+    print(f'Model checkpoints saved at: {ckpt_path}')
 
 
 if __name__ == '__main__':
